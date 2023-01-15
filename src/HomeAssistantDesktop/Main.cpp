01a7fbd248b31b0686e56d9ae69e4462d2ce7618
Hernan Martinez <hernan.c.martinez@gmail.com>
2023-01-14 17:01:22 -0700

    Add WinApi dep to HADesktop

--- src/HomeAssistantDesktop/Main.cpp
+++ src/HomeAssistantDesktop/Main.cpp
@@ -1,14 +1,18 @@
 #include <QApplication>
 #include "TrayViewModel.h"
 #include "TrayView.h"
+#include "WinApi.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    
+    WinApi_Shutdown();
+    WinApi_Initialize();
 
     TrayViewModel viewModel;
     TrayView view(&viewModel);
 
-    return a.exec();
+    auto ret = a.exec();
+    WinApi_Shutdown();
+    return ret;
 }
